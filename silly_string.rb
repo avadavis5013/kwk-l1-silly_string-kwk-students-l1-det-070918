e5563842e4b871e967388bb2251d2eacd35a171b
avadavis5013 <adavis5013@gmail.com>
2018-07-10 18:11:15 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,15 +1,17 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "hello"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is hello"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "my favorite word is hi".upcase
+
+puts "MY FAVORITE WORD IS #{HI}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
@@ -49,3 +51,6 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 sentence = sentence # Add your gsub chain here
 
 puts sentence
+
+puts "my favorite word is hi".upcase
+
